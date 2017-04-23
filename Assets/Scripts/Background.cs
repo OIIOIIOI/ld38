3409377cf4f110336c1508a35591f351b81266d1
01101101 <sylvere.armange@gmail.com>
2017-04-23 22:42:53 +0200

    background alts WIP

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+
+    public Sprite[] alts;
+
+    SpriteRenderer sr;
+    Color alphaWhite = new Color(1, 1, 1, 0);
+
+    private void Awake ()
+    {
+        sr = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    public void Swap (int index = 0)
+    {
+        StopAllCoroutines();
+
+        if (sr != null && alts.Length > index && alts[index] != null)
+            StartCoroutine(SwapSprite(alts[index]));
+    }
+
+    IEnumerator SwapSprite (Sprite s)
+    {
+        sr.color = alphaWhite;
+        sr.sprite = s;
+        sr.color = Color.white;
+        yield return new WaitForSeconds(0.1f);
+        sr.color = alphaWhite;
+        yield return null;
+    }
+
+}
---
+++ Assets/Scripts/Bathroom.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bathroom : MonoBehaviour
+{
+
+    public Background background;
+
+    private void Start ()
+    {
+        InvokeRepeating("Lightning", 3f, 3f);
+    }
+
+    void Lightning ()
+    {
+        background.Swap();
+    }
+
+}
