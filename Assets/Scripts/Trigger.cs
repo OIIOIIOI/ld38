71c73e0c0dda0bd7d059adf625f42f572952aaa2
01101101 <sylvere.armange@gmail.com>
2017-04-23 17:14:02 +0200

    tons of stuff

---
+++ Assets/Scripts/Trigger.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Trigger : MonoBehaviour
+{
+
+    public string hoverText;
+
+    public TargetScene targetScene;
+
+    public enum TargetScene
+    {
+        Lobby, LobbyExitDoor,
+        Bathroom,
+        Stairs, StairsCompass,
+        CompassRoom,
+        Chapel, ChapelCircle,
+        WhiteRoom,
+        Library, LibraryCompass,
+        BlueRoom, BlueRoomCompass,
+        ColumnsRoom, ColumnsRoomDetail
+    }
+
+    Text cursorText;
+
+	void Start ()
+    {
+        GameObject go = GameObject.Find("CursorText");
+        if (go)
+        {
+            cursorText = go.GetComponent<Text>();
+            cursorText.text = "";
+        }
+	}
+
+    private void OnMouseEnter ()
+    {
+        if (cursorText)
+            cursorText.text = hoverText;
+    }
+
+    private void OnMouseOver ()
+    {
+        if (cursorText)
+            cursorText.gameObject.transform.position = Input.mousePosition;
+    }
+
+    private void OnMouseExit ()
+    {
+        if (cursorText)
+            cursorText.text = "";
+    }
+
+    private void OnMouseDown ()
+    {
+        if (cursorText)
+            cursorText.text = "";
+        SceneManager.LoadScene(targetScene.ToString());
+    }
+
+}
---
+++ Assets/Scripts/TriggerManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerManager : MonoBehaviour
+{
+
+    public BoxCollider2D[] triggers;
+
+    public Texture2D cursor;
+
+	void Start ()
+    {
+        Cursor.SetCursor(cursor, Vector2.zero, CursorMode.Auto);
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+}
